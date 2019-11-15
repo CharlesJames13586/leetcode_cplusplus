d528b132b854dab6fc2f4c6f2b4d7d8aca33390f
WangShaopeng <39290680+CharlesJames13586@users.noreply.github.com>
2019-11-15 20:20:22 +0800

    提交18.md

---
+++ Algorithm/19-RemoveNthNodeFromEndofList.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+ //Definition for singly-linked list.
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+	}
+};
+//用于测试的主函数
+int main() {
+
+}
