6806e5a94d18183bcfef4745d3459e6deab19747
WangShaopeng <39290680+CharlesJames13586@users.noreply.github.com>
2019-11-15 22:10:30 +0800

    提交19的代码和md文件

--- Algorithm/19-RemoveNthNodeFromEndofList.cpp
+++ Algorithm/19-RemoveNthNodeFromEndofList.cpp
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-
+struct ListNode;
+void printList(ListNode *head);
  //Definition for singly-linked list.
  struct ListNode {
      int val;
@@ -16,10 +17,67 @@ using namespace std;
 class Solution {
 public:
 	ListNode* removeNthFromEnd(ListNode* head, int n) {
-
+		//加一个空节点，这样删除第一个节点和其它节点的操作就一样了
+		ListNode header = ListNode(0);
+		header.next = head;
+		head = &header;
+		//采用双指针法
+		ListNode * temp = head;
+		ListNode * last = head;
+		//先将last往前走n步
+		for (int i = 0; i < n+1; i++) {
+			last = last->next;
+		}
+		//然后temp和last一起往后走，知道last走到最后，这时temp指向倒数第n个节点
+		while (last != NULL) {
+			temp = temp->next;
+			last = last->next;
+		}
+		ListNode *del = temp->next;
+		temp->next = temp->next->next;   //删除倒数第n个节点
+		delete del;          //释放内存空间
+		return head->next;
 	}
 };
 //用于测试的主函数
 int main() {
+	ListNode head = ListNode(0);
+	ListNode * temp = &head;
+	int l = 0;
+	cout << "请输入链表的长度：";
+	cin >> l;
+	cout << endl;
+	int nodeVal = 0;
+	for (int i = 0; i < l; i++) {
+		cin >> nodeVal;
+		temp->val = nodeVal;
+		if (i != l-1) {
+			temp->next = new ListNode(0);
+			temp = temp->next;
+		}
+	}
+	printList(&head);
+	int n = 0;
+	cout << "请输入要删除倒数第n个节点的n的值：";
+	cin >> n;
+	cout << endl;
+	Solution solution;
+	temp = solution.removeNthFromEnd(&head, n);
+	printList(temp);
+	return 0;
+}
 
+//输出链表
+void printList(ListNode *head) {
+	ListNode * temp = head;
+	while (temp != NULL) {
+		cout << temp->val;
+		temp = temp->next;
+		if (temp != NULL) {
+			cout << "->";
+		}
+		else {
+			cout << endl;
+		}
+	}
 }
--- Algorithm/19-RemoveNthNodeFromEndofList.md
+++ Algorithm/19-RemoveNthNodeFromEndofList.md
@@ -1,12 +1,33 @@
-### 19Remove Nth Node From End of List
+### 19.Remove Nth Node From End of List
 * Given a linked list, remove the n-th node from the end of list and return its head.
+
+  给一个链表，删除倒数第n个节点然后返回链表的头。
 * **Example:**
   >Given linked list: 1->2->3->4->5, and n = 2.<br>
-  After removing the second node from the end, the linked list becomes 1->2->3->5.
+  After removing the second node from the end, the linked list becomes 1->2->3->5.<br>
+
+  >给一个链表：1->2->3->4->5，和变量n = 2。<br>
+  >然后删除倒数第二个节点，原来的链表变为：1->2->3->5。<br>
   
 * **Note:**
 
   Given n will always be valid.
+
+  给出的变量n都是合法的。
+
 * **Follow up:**
 
   Could you do this in one pass?
+
+  你能一次通过吗？
+
+* **解题思路**
+
+  &emsp;&emsp;采用双指针法，使用last(用来检测是否扫描到链表的最后)，temp两个指针。在刚开始的时候，
+  先让last扫描n+1个节点，然后temp和last一起向后扫描，待last扫到最后一个节点，temp节点
+  指向了倒数n+1个节点，然后删除temp后继节点。为了让删除首元节点和其它节点操作相同，我
+  们在链表前添加了一个附加空节点。
+
+  &emsp;&emsp;本算法可以只扫描一遍就删除了倒数第n个节点，但是在实际运行中因为同时更新两个节点，和
+  先扫描一遍链表求链表大小，再扫描一遍删除倒数第n个的方法实际运行时间差不多。但是该方
+  法相对而言比较新颖一点。
